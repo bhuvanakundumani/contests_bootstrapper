62b22ce6a053e5156de54a1146ecb4b72aacf117
bhuvanakundumani <bhuvana.kundumani@gmail.com>
2018-10-25 20:29:35 +0530

    changes - comments removed, functions added

--- pascals_triangle/pascals_triangle.py
+++ pascals_triangle/pascals_triangle.py
@@ -1,26 +1,40 @@
-
-"""
-Pascal's triangle is a triangular array of numbers in which those at the ends of the rows are 1 and each of the others is the sum of the nearest two numbers in the row above (the apex, 1, being at the top).
+def print_pascalstriangle(a):
     
-"""
-print("Welcome to Pascal's traingle:")
-
-
-n=int(input("Enter number of rows for the pascal's traingle: "))
-a=[]
-for i in range(n):
-    a.append([])
-    a[i].append(1)
-    for j in range(1,i):
-        a[i].append(a[i-1][j-1]+a[i-1][j])
-    if(n!=0):
-        a[i].append(1)
-		
-		
-# printing pascal's triangle
-print( " Your Pascal's traiange for the number {}".format(n))
-for i in range(n):
-    print("   "*(n-i),end=" ",sep=" ")
-    for j in range(0,i+1):
-        print('{0:6}'.format(a[i][j]),end=" ",sep=" ")
+    n = len(a)
+    # printing pascal's triangle
     print()
+    print( "Your Pascal's triangle for the number {}".format(n))
+    print()
+    for i in range(n):
+        print("   "*(n-i),end=" ",sep=" ")
+        for j in range(0,i+1):
+            print('{0:6}'.format(a[i][j]),end=" ",sep=" ")
+        print()
+
+
+
+
+
+def pascals_triangle(n):
+    a=[]
+    for i in range(n):
+        a.append([])
+        a[i].append(1)
+        for j in range(1,i):
+            a[i].append(a[i-1][j-1]+a[i-1][j])
+        if(n!=0):
+            a[i].append(1)
+    return a
+
+
+
+
+print("Welcome to Pascal's triangle:")
+
+
+n=int(input("Enter number of rows for the pascal's triangle: "))
+b=[]
+# pascal's triangle is generated
+b = pascals_triangle(n)
+# pascal's triangle gets printed here
+print_pascalstriangle(b)
